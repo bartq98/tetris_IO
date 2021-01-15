bc4fb782031fa0a3634021493028fbc709f2784c
bartq98 <bartusiekkrosno@poczta.fm>
2021-01-15 03:02:05 +0100

    Add tests for Gameboard.is_row_fully_filled()
    Cases:
      - fully filled row
      - fully empty row (all blocks within row are EMPTY_BLOCK)
      - some blocks (one, few, all except one) are FALLEN_BLOCK (filling
      row partialy)

--- gameboardUnittest.py
+++ gameboardUnittest.py
@@ -53,7 +53,35 @@ class TestGameboardMethods(unittest.TestCase):
 
     def test_is_row_fully_filled(self):
         gb = gameboard.Gameboard()
+        # It has no sense if bottom row is filled (it is built by BORDER_BLOCK and filled means built only by FALLEN_BLOCK)
         self.assertFalse(gb.is_row_fully_filled(gb.fields[config.BOARD_BOTTOM_ROW]))
+        
+        # The rest rows also should return False as new gameboard should have only empty rows (and one border at bottom)
+        for row in gb.fields:
+            self.assertFalse(gb.is_row_fully_filled(row))
+
+        for index in range(config.BOARD_FIRST_COLUMN, config.BOARD_LAST_COLUMN+1):
+            gb.fields[0][index] = config.FALLEN_BLOCK
+        
+        self.assertTrue(gb.is_row_fully_filled(gb.fields[0]))
+
+        # Return one block to EMPTY_BLOCK to check is row not fully filled
+        gb.fields[0][4] = config.EMPTY_BLOCK
+        self.assertFalse(gb.is_row_fully_filled(gb.fields[0]))
+
+        # Next row has 4 blocks filled (FALLEN_BLOCK) and rest empty
+        for index in range(config.BOARD_FIRST_COLUMN, config.BOARD_LAST_COLUMN+1, 3):
+            gb.fields[1][index] = config.FALLEN_BLOCK
+
+        self.assertFalse(gb.is_row_fully_filled(gb.fields[1]))
+
+        # This row has only one block FALLEN
+        gb.fields[2][1] = config.FALLEN_BLOCK
+        self.assertFalse(gb.is_row_fully_filled(gb.fields[2]))
+
+
+
+
 
 
 if __name__ == "__main__":
